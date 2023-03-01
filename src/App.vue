acb6d920aa4bdcb489a0acfbff80a923ecc469fc
Jerry Seyi <jerryseyi68@gmail.com>
2023-03-01 20:18:29 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import BlogPost from "./components/blog-post.vue";
+
+export default {
+  name: 'App',
+  components: {
+    BlogPost
+  },
+  data() {
+    return {
+      authenticated: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-center flex-col">
+    <h3 class="text-3xl flex items-center justify-center mb-10">Welcome to snoopy blog</h3>
+    <blog-post :auth="authenticated"></blog-post>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/blog-post.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-show="auth" class="flex items-center justify-center">
+    <div class="max-w-7xl">
+      <h3 class="uppercase text-2xl font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "blog-post",
+  props: {
+    auth: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
